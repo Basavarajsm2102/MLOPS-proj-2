04eb5b0e48ac608a2a10fb49fb934ed1bd050078
Basavaraj Madiwalar <basavarajmadiwalar@basavaraj.local>
2025-05-19 19:05:31 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Basavaraj SM",
+    author_email="basavarajsm2102@gmail.com",
+    packages=find_packages()
+)
